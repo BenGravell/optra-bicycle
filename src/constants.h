464131d34fbd13a3d503a22b7a5811ad9d3b96e5
Ben Gravell <bjgravell@gmail.com>
2025-03-23 08:31:44 +0100

    constants

--- src/constants.h
+++ src/constants.h
@@ -8,3 +8,9 @@ static const double LOG2 = std::log(2.0);
 // static constexpr double PI = std::numbers::pi;
 // static constexpr double RAD2DEG = 180.0 / std::numbers::pi;
 // static constexpr double DEG2RAD = std::numbers::pi / 180.0;
+
+
+// TODO move to a config struct
+static constexpr double max_lon_accel = 3.0;
+static constexpr double max_lat_accel = 6.0;
+static constexpr double max_curvature = 0.25;
